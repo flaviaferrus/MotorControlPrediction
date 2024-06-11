f6efbf6c263298bdec9a3b653c658e5b8482e4b8
flaviaferrus <flaviaferrus@gmail.com>
2024-06-11 17:51:37 +0200

    Update main file

--- Implementation/main_multiple.py
+++ Implementation/main_multiple.py
@@ -10,7 +10,7 @@ from typing import Tuple
 # Load the custom functions from .py file  
 from utils_data_multiple import load_multiple_data, plot_multiple_data, point_to_segment, cleaning_clustering_multiple_data
 from utils_data_multiple import saving_processed_mult_data, load_processed_mult_data, multiple_linear_transf
-from utils_model_multiple import fitParamaters_mult, saving_new_params
+from utils_model_multiple import fitParamaters_mult, load_params
 
 ##############################################
 ## GENERAL PROCESSING AND FITTING FUNCTIONS ##
@@ -53,7 +53,7 @@ def processing_data(n_clusters = 4, path = 'dataTrajectories') -> Tuple[pd.DataF
     
     return scaled_data_dict, results_dict, idxrule_dict, segments 
 
-def main(processing = False, n_clusters = 4): 
+def main(processing = False, fitting = False, n_clusters = 4): 
     
     if processing: 
         print('Loading and processing data...')
@@ -71,22 +71,24 @@ def main(processing = False, n_clusters = 4):
             (-10.5, 1.25)  # pt0
         ]
         segments = point_to_segment(cluster_points, n_clusters)
-        
-        
+                
     print('Data loaded and processed :)')
     print('Fitting paramaters for the optimized trajectory...')   
     
-    new_params, opt_sigma = fitParamaters_mult(scaled_data_dict,
+    
+    if fitting: 
+        new_params, opt_sigma = fitParamaters_mult(scaled_data_dict,
                   idxrule_dict, 
                   results_dict,
                   segments, 
                   first_subj = 25, last_subj = 30,
                   n_clusters = 4, folder_name = 'fitted_trajectories_', 
-                  saving = True)   
-    
-    saving_new_params(new_params, folder_name= 'fitted_params')
-    saving_new_params(opt_sigma, folder_name= 'fitted_params_sigma')
-      
+                  saving = True, saving_data = True)   
+    else: 
+        params_loaded = load_params(folder_name = 'fitted_params')
+        opt_sigma = load_params(folder_name = 'fitted_params_sigma')
+
+    print('Parameters fitteg correctly.')
 
 if __name__ == '__main__':
-    main(loading = False)
+    main(processing = False, fitting = False)
